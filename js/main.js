9360547bdbe0c05b579ac78ddc8bb1dd5fb76212
okerivy <okerivy@163.com>
2022-01-09 17:11:24 +0800

    格式化

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let OS = {windows:"windows", mac:"mac",other:"other", userText: "text"}
+let OS = { windows: "windows", mac: "mac", other: "other", userText: "text" };
 var myOs = OS.windows; // default
 
 var prefixKeys = "";
@@ -6,255 +6,259 @@ var prefixKeysHtml = "";
 
 var isTextInputMode = false;
 
-$('document').ready(function () {
-    if(platform.os.family.indexOf("Win") != -1) {
-        setCustomOS(OS.windows);
-    } else if(platform.os.family.indexOf("Mac") != -1) {
-        setCustomOS(OS.mac);
+$("document").ready(function () {
+  if (platform.os.family.indexOf("Win") != -1) {
+    setCustomOS(OS.windows);
+  } else if (platform.os.family.indexOf("Mac") != -1) {
+    setCustomOS(OS.mac);
+  } else {
+    setCustomOS(OS.other);
+  }
+
+  // on OS selection radio button click
+  $("input:radio").click((e) => {
+    setCustomOS(e.currentTarget.name);
+  });
+
+  $("#user-text-input").hide();
+
+  function setCustomOS(newOs) {
+    myOs = newOs;
+    $(":radio").prop("checked", false); // clear previous os selections
+    $("input[name=" + myOs + "]:radio").prop("checked", true);
+
+    if (myOs == OS.userText) {
+      $("#kb-input").hide();
+      $("#user-text-input").show();
+      isTextInputMode = true;
     } else {
-        setCustomOS(OS.other);
+      $("#kb-input").show();
+      $("#user-text-input").hide();
+      isTextInputMode = false;
     }
 
-    // on OS selection radio button click
-    $("input:radio").click((e)=>{
-        setCustomOS(e.currentTarget.name);
+    updateToggleButtons(myOs);
+  }
+
+  function updateToggleButtons(newOs) {
+    let $metaKey = $("#meta-toggle");
+    $metaKey.removeClass("cmd");
+    $metaKey.removeClass("win");
+    $metaKey.removeClass("meta");
+
+    if (myOs == OS.mac) {
+      $metaKey.addClass("cmd");
+    } else if (myOs == OS.windows) {
+      $metaKey.addClass("win");
+    } else {
+      $metaKey.addClass("meta");
+    }
+  }
+
+  const keyMap = {
+    k32: "SPACE",
+    k65: "A",
+    k66: "B",
+    k67: "C",
+    k68: "D",
+    k69: "E",
+    k70: "F",
+    k71: "G",
+    k72: "H",
+    k73: "I",
+    k74: "J",
+    k75: "K",
+    k76: "L",
+    k77: "M",
+    k78: "N",
+    k79: "O",
+    k80: "P",
+    k81: "Q",
+    k82: "R",
+    k83: "S",
+    k84: "T",
+    k85: "U",
+    k86: "V",
+    k87: "W",
+    k88: "X",
+    k89: "Y",
+    k90: "Z",
+  };
+
+  $("#img-preview-div").hide();
+
+  $("#save-btn").click(function () {
+    var e = document.getElementById("output-location");
+    var e_width = e.offsetWidth;
+    var e_height = e.offsetHeight;
+    var e_x_offset = window.scrollX + e.getBoundingClientRect().left;
+    var e_y_offset = window.scrollY + e.getBoundingClientRect().top;
+
+    html2canvas(e, {
+      scale: 1,
+      backgroundColor: null,
+      width: e_width,
+      height: e_height,
+      x: e_x_offset,
+      y: e_y_offset,
+    }).then((canvas) => {
+      //document.body.appendChild(canvas)
+      $("#img-preview-div").show();
+      var base64image = canvas.toDataURL("image/png");
+      let $imgDiv = $('<img src="' + base64image + '"/>');
+      $("#img-out-preview").empty();
+      $("#img-out-preview").append($imgDiv);
+      // var win = window.open('', "_blank");
+      // win.document.write('<img src="' + base64image + '"/>');
+      // win.document.close();
     });
+  });
 
-    $('#user-text-input').hide();
+  $("#meta-toggle").click((e) => {
+    if ($(e.currentTarget).hasClass("keyboard-keydown")) {
+      $(e.currentTarget).removeClass("keyboard-keydown");
+      prefixKeys = "";
+      prefixKeysHtml = "";
+    } else {
+      $(e.currentTarget).addClass("keyboard-keydown");
 
-    function setCustomOS(newOs) {
-        myOs = newOs;
-        $(":radio").prop("checked", false); // clear previous os selections
-        $("input[name="+myOs+"]:radio").prop("checked", true);
-
-        if (myOs == OS.userText) {
-            $('#kb-input').hide();
-            $('#user-text-input').show();
-            isTextInputMode = true;
-        } else {
-            $('#kb-input').show();
-            $('#user-text-input').hide();
-            isTextInputMode = false;
-        }
-
-        updateToggleButtons(myOs);
+      if (myOs == OS.mac) {
+        prefixKeys = "⌘ Cmd + ";
+        prefixKeysHtml = "<kbd>⌘ Cmd</kbd>+";
+      } else if (myOs == OS.windows) {
+        prefixKeys = "⊞ Win + ";
+        prefixKeysHtml = "<kbd>⊞ Win</kbd>+";
+      } else {
+        prefixKeys = "Meta + ";
+        prefixKeysHtml = "<kbd>Meta</kbd>+";
+      }
     }
 
-    function updateToggleButtons(newOs) {
-        let $metaKey = $('#meta-toggle');
-        $metaKey.removeClass('cmd');
-        $metaKey.removeClass('win');
-        $metaKey.removeClass('meta');
+    $("#kb-input").val(prefixKeys);
+    $("#output-location").html(prefixKeysHtml);
+  });
 
-        if (myOs == OS.mac) {
-            $metaKey.addClass('cmd');
-        } else if (myOs == OS.windows) {
-            $metaKey.addClass('win');
+  $("body").keyup(function (e) {
+    if (isTextInputMode) {
+      // allow user to input any text
+      var userText = $("#user-text-input").val();
+      console.log("userText=" + userText);
+
+      // remove all spaces
+      userText = userText.replaceAll(" ", "");
+
+      // split by +
+      var splitKeys = userText.split("+");
+
+      var kbString = "";
+      var kbHtmlString = "";
+      for (var i = 0; i < splitKeys.length; i++) {
+        if (splitKeys[i].indexOf(",") != -1) {
+          var commaSplits = splitKeys[i].split(",");
+          for (var j = 0; j < commaSplits.length; j++) {
+            kbString += commaSplits[j];
+            kbHtmlString += "<kbd>" + commaSplits[j] + "</kbd>";
+
+            if (j == commaSplits.length - 1) {
+              // last one - no need to add +
+            } else {
+              kbString += " , ";
+              kbHtmlString += ",";
+            }
+          }
         } else {
-            $metaKey.addClass('meta');
+          kbString += splitKeys[i];
+          kbHtmlString += "<kbd>" + splitKeys[i] + "</kbd>";
+
+          if (i == splitKeys.length - 1) {
+            // last one - no need to add +
+          } else {
+            kbString += " + ";
+            kbHtmlString += "+";
+          }
         }
+      }
+
+      $("#output-location").html(kbHtmlString);
+      return;
+    }
+  });
+
+  $("body").keydown(function (e) {
+    if (isTextInputMode) {
+      // // allow user to input any text
+      // var userText = $('#user-text-input').val();
+      // console.log("userText="+userText);
+
+      // // remove all spaces
+      // userText = userText.replaceAll(" ", "")
+
+      // // split by +
+      // var splitKeys = userText.split("+")
+
+      // var kbString = '';
+      // var kbHtmlString = '';
+      // for (var i=0; i<splitKeys.length; i++) {
+      //     kbString += splitKeys[i] + ' + ';
+      //     kbHtmlString += '<kbd>'+splitKeys[i]+'</kbd>+';
+      // }
+
+      // $('#output-location').html(kbHtmlString);
+      return;
+    }
+    const id = "k" + e.keyCode;
+    console.log("down = " + keyMap[id] + ", keycode = " + e.keyCode);
+
+    var kbString = prefixKeys + "";
+    var kbHtmlString = prefixKeysHtml + "";
+
+    if (e.ctrlKey) {
+      kbString += "Ctrl + ";
+      kbHtmlString += "<kbd>Ctrl</kbd>+";
     }
 
-    const keyMap = {
-        k32: 'SPACE',
-        k65: 'A',
-        k66: 'B',
-        k67: 'C',
-        k68: 'D',
-        k69: 'E',
-        k70: 'F',
-        k71: 'G',
-        k72: 'H',
-        k73: 'I',
-        k74: 'J',
-        k75: 'K',
-        k76: 'L',
-        k77: 'M',
-        k78: 'N',
-        k79: 'O',
-        k80: 'P',
-        k81: 'Q',
-        k82: 'R',
-        k83: 'S',
-        k84: 'T',
-        k85: 'U',
-        k86: 'V',
-        k87: 'W',
-        k88: 'X',
-        k89: 'Y',
-        k90: 'Z',
-    };
+    if (e.altKey) {
+      kbString += "Alt + ";
+      kbHtmlString += "<kbd>Alt</kbd>+";
+    }
 
-    $('#img-preview-div').hide();
+    if (e.shiftKey) {
+      kbString += "Shift + ";
+      kbHtmlString += "<kbd>Shift</kbd>+";
+    }
 
-    $('#save-btn').click(function () {
-      var e = document.getElementById("output-location");
-      var e_width = e.offsetWidth;
-      var e_height = e.offsetHeight;
-      var e_x_offset = window.scrollX + e.getBoundingClientRect().left;
-      var e_y_offset = window.scrollY + e.getBoundingClientRect().top;
+    if (e.metaKey) {
+      if (myOs == OS.mac) {
+        kbString += "⌘ Cmd + ";
+        kbHtmlString += "<kbd>⌘ Cmd</kbd>+";
+      } else if (myOs == OS.windows) {
+        kbString += "⊞ Win + ";
+        kbHtmlString += "<kbd>⊞ Win</kbd>+";
+      } else {
+        kbString += "Meta + ";
+        kbHtmlString += "<kbd>Meta</kbd>+";
+      }
+    }
 
-      html2canvas(e, {
-        scale: 1,
-        backgroundColor: null,
-        width: e_width,
-        height: e_height,
-        x: e_x_offset,
-        y: e_y_offset }).then(canvas => {
-            //document.body.appendChild(canvas)
-            $('#img-preview-div').show();
-            var base64image = canvas.toDataURL("image/png");
-            let $imgDiv = $('<img src="' + base64image + '"/>');
-            $('#img-out-preview').empty();
-            $('#img-out-preview').append($imgDiv);
-            // var win = window.open('', "_blank");
-            // win.document.write('<img src="' + base64image + '"/>');
-            // win.document.close();
-        });
-    });
+    if (
+      e.key != "Control" &&
+      e.key != "Shift" &&
+      e.key != "Alt" &&
+      e.key != "Meta"
+    ) {
+      kbString += e.key;
+      console.log(e.key);
+      if (keyMap[id] != undefined) {
+        kbHtmlString += "<kbd>" + keyMap[id] + "</kbd>";
+      } else {
+        kbHtmlString += "<kbd>" + e.key + "</kbd>";
+      }
+    }
 
-    $('#meta-toggle').click((e)=>{
-        if ($(e.currentTarget).hasClass('keyboard-keydown')) {
-            $(e.currentTarget).removeClass('keyboard-keydown');
-            prefixKeys = '';
-            prefixKeysHtml = '';
-        } else {
-            $(e.currentTarget).addClass('keyboard-keydown');
+    $("#kb-input").val(kbString);
+    $("#output-location").html(kbHtmlString);
 
-            if (myOs == OS.mac) {
-                prefixKeys = '⌘ Cmd + ';
-                prefixKeysHtml = '<kbd>⌘ Cmd</kbd>+';
-            } else if (myOs == OS.windows) {
-                prefixKeys = '⊞ Win + ';
-                prefixKeysHtml = '<kbd>⊞ Win</kbd>+';
-            } else {
-                prefixKeys = 'Meta + ';
-                prefixKeysHtml = '<kbd>Meta</kbd>+';
-            }
-        }
-
-        $('#kb-input').val(prefixKeys);
-        $('#output-location').html(prefixKeysHtml);
-    });
-
-    $('body').keyup(function (e) {
-        if (isTextInputMode) {
-            // allow user to input any text
-            var userText = $('#user-text-input').val();
-            console.log("userText="+userText);
-            
-            // remove all spaces
-            userText = userText.replaceAll(" ", "")
-
-            // split by +
-            var splitKeys = userText.split("+")
-
-            var kbString = '';
-            var kbHtmlString = '';
-            for (var i=0; i<splitKeys.length; i++) {
-                if (splitKeys[i].indexOf(',') != -1) {
-                    var commaSplits = splitKeys[i].split(',');
-                    for(var j=0; j<commaSplits.length;j++) {
-                        kbString += commaSplits[j];
-                        kbHtmlString += '<kbd>'+commaSplits[j]+'</kbd>';
-
-                        if (j == commaSplits.length - 1) {
-                            // last one - no need to add +
-                        } else {
-                            kbString += ' , ';
-                            kbHtmlString += ',';
-                        }
-                    }
-                } else {
-                    kbString += splitKeys[i];
-                    kbHtmlString += '<kbd>'+splitKeys[i]+'</kbd>';
-    
-                    if (i == splitKeys.length - 1) {
-                        // last one - no need to add +
-                    } else {
-                        kbString += ' + ';
-                        kbHtmlString += '+';
-                    }
-                }
-            }
-
-            $('#output-location').html(kbHtmlString);
-            return;
-        }
-    });
-
-    $('body').keydown(function (e) {
-        if (isTextInputMode) {
-            // // allow user to input any text
-            // var userText = $('#user-text-input').val();
-            // console.log("userText="+userText);
-            
-            // // remove all spaces
-            // userText = userText.replaceAll(" ", "")
-
-            // // split by +
-            // var splitKeys = userText.split("+")
-
-            // var kbString = '';
-            // var kbHtmlString = '';
-            // for (var i=0; i<splitKeys.length; i++) {
-            //     kbString += splitKeys[i] + ' + ';
-            //     kbHtmlString += '<kbd>'+splitKeys[i]+'</kbd>+';
-            // }
-
-            // $('#output-location').html(kbHtmlString);
-            return;
-        }
-        const id = 'k'+e.keyCode;
-        console.log('down = ' + keyMap[id]+ ", keycode = "+e.keyCode);
-
-        var kbString = prefixKeys + '';
-        var kbHtmlString = prefixKeysHtml + '';
-
-        if (e.ctrlKey) {
-            kbString += 'Ctrl + ';
-            kbHtmlString += '<kbd>Ctrl</kbd>+';
-        }
-
-        if(e.altKey){
-            kbString += 'Alt + ';
-            kbHtmlString += '<kbd>Alt</kbd>+';
-        }
-
-        if (e.shiftKey) {
-            kbString += 'Shift + ';
-            kbHtmlString += '<kbd>Shift</kbd>+';
-        }
-
-        if (e.metaKey) {
-            if (myOs == OS.mac) {
-                kbString += '⌘ Cmd + ';
-                kbHtmlString += '<kbd>⌘ Cmd</kbd>+';
-            } else if (myOs == OS.windows) {
-                kbString += '⊞ Win + ';
-                kbHtmlString += '<kbd>⊞ Win</kbd>+';
-            } else {
-                kbString += 'Meta + ';
-                kbHtmlString += '<kbd>Meta</kbd>+';
-            }
-        }
-
-        if (e.key != 'Control' && e.key != 'Shift' && e.key != 'Alt' && e.key != 'Meta') {
-            kbString += e.key;
-            console.log(e.key);
-            if (keyMap[id] != undefined) {
-                kbHtmlString += '<kbd>' + keyMap[id] + '</kbd>';    
-            } else {
-                kbHtmlString += '<kbd>' + e.key + '</kbd>';
-            }
-        }
-
-        $('#kb-input').val(kbString);
-        $('#output-location').html(kbHtmlString);
-        
-        e.preventDefault();
-        e.stopPropagation();
-    });
-
+    e.preventDefault();
+    e.stopPropagation();
+  });
 });
-
